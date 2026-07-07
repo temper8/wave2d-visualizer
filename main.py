065cf3cebafca39516f1bab8e46c012b8a31d83b
Alex <temper8@gmail.com>
2026-07-07 17:04:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from utils import dataset_reader, view2d
 def main():
     print("Hello from wave2d-visualizer!")
     file_path = 'results.h5'
-
-    R = dataset_reader(file_path, '/nphi-122/grid_2d/X')
-    Z = dataset_reader(file_path, '/nphi-122/grid_2d/Y')
+    nphi = "nphi-122"
+    R = dataset_reader(file_path, '/coord/X')
+    Z = dataset_reader(file_path, '/coord/Y')
 
     #R = dataset_reader(file_path, '/flux_surf_2D/X')
     #Z = dataset_reader(file_path, '/flux_surf_2D/Y')
@@ -13,12 +13,12 @@ def main():
 
     theta_deg = dataset_reader(file_path, '/flux_surf_2D/theta_deg')
     view2d(R, Z, theta_deg, "theta_deg")
-    #Ea_field = dataset_reader(file_path, '/nphi-122/field_2d/Ea')
-    #view2d(R, Z, Ea_field, "Ea field")
+    Ea_field = dataset_reader(file_path, f'/{nphi}/field_2d/Ea')
+    view2d(R, Z, Ea_field, "Ea field")
 
 
-    R = dataset_reader(file_path, '/plasma_par_2D/X')
-    Z = dataset_reader(file_path, '/plasma_par_2D/Y')
+    #R = dataset_reader(file_path, '/plasma_par_2D/X')
+    #Z = dataset_reader(file_path, '/plasma_par_2D/Y')
     Te_2D = dataset_reader(file_path, '/plasma_par_2D/Te')
     view2d(R, Z, Te_2D, "Te")
 
