5e7b211564e5cea7009639cc7e139871bde35685
Alex <temper8@gmail.com>
2026-07-14 13:40:27 +0300

    Update converter_to_hdf.py

--- converter_to_hdf.py
+++ converter_to_hdf.py
@@ -8,7 +8,8 @@ from tqdm import tqdm
 files ={
     "polar_mesh" : 'Elmfire_WagD/polar_mesh_z1.dat',
     'rho_mesh' : 'Elmfire_WagD/rho_mesh_z1.dat',
-    'time' : 'Elmfire_WagD/time_z1.dat'
+    'time' : 'Elmfire_WagD/time_z1.dat',
+    'data' : 'Elmfire_WagD/nep_z1.dat',
 }
 
 polar_mesh = pd.read_csv(files['polar_mesh'], sep=r'\s+')
@@ -33,12 +34,18 @@ print(df_times.head())
 print(df_times.dtypes)
 time_indices = np.array(df_times.index, dtype=np.int32)
 N_time = len(time_indices)
+N_rho= len(rho_1d)
+N_theta = N_theta_1d[-1]
 
+print(f'data size = {N_time} x {N_rho} x {N_theta}')
 time_values = df_times.loc[time_indices, 'time_value'].values.astype(np.float32)
 
 
 
 
+
+column_names = ['time_index', 'rho', 'theta', 'd_dens']
+df_dens = pd.read_csv(files['data'], sep=r'\s+', header=None, names=column_names, dtype={'time_index': int})
 
 output_h5 = 'z1.h5'
 with h5py.File(output_h5, 'w') as f:
@@ -47,3 +54,13 @@ with h5py.File(output_h5, 'w') as f:
     f.create_dataset('rho_mesh/N_theta', data=N_theta_1d)
     f.create_dataset('time_indices', data=time_indices)
     f.create_dataset('time_values', data=time_values)
+
+    ds_fluct = f.create_dataset('fluctuations', shape=(N_time, N_rho, N_theta), 
+                                    dtype='float32', compression='gzip', chunks=(1, N_rho, N_theta))
+    
+
+        
+    # Заполняем куб пошагово, чтобы не держать в памяти гигантские массивы
+    for t_idx, time_val in enumerate(time_indices):
+        step_data = df_dens[df_dens['time_index'] == time_val]['rho'].values
+        ds_fluct[t_idx] = step_data.reshape(N_rho, N_theta)    
