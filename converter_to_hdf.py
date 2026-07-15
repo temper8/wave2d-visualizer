e5ac3e80eb953ff52c0cba64999a3b21d6dadf74
Alex <temper8@gmail.com>
2026-07-15 08:30:56 +0300

    now convert all file

--- converter_to_hdf.py
+++ converter_to_hdf.py
@@ -22,7 +22,7 @@ print(polar_mesh.dtypes)
 print(f"Размер данных: {polar_mesh.shape}")
 print(f"Количество строк: {polar_mesh.shape[0]}")
 print(f"Количество столбцов: {polar_mesh.shape[1]}")
-step_nrows = polar_mesh.shape[0]
+chunksize = polar_mesh.shape[0]
 
 rho_mesh = pd.read_csv(files['rho_mesh'], sep=r'\s+')
 rho_1d = np.array(rho_mesh['rho(cm)'], dtype=np.float32)
@@ -43,13 +43,9 @@ N_theta = N_theta_1d[-1]
 print(f'data size = {N_time} x {N_rho} x {N_theta}')
 time_values = df_times.loc[time_indices, 'time_value'].values.astype(np.float32)
 
-
-
-
-
 column_names = ['time_index', 'rho', 'theta', 'd_dens']
 df_dens = pd.read_csv(files['data'], sep=r'\s+', header=None, names=column_names, 
-                      dtype={'time_index': int}, nrows=step_nrows)
+                      dtype={'time_index': int}, nrows=chunksize)
 
 output_h5 = 'z1.h5'
 with h5py.File(output_h5, 'w') as f:
@@ -62,22 +58,22 @@ with h5py.File(output_h5, 'w') as f:
     ds_fluct = f.create_dataset('fluctuations', shape=(N_time, N_rho, N_theta), 
                                     dtype='float32', compression='gzip', chunks=(1, N_rho, N_theta))
     
-
     theta = np.linspace(0,2*np.pi, N_theta)
     # Заполняем куб пошагово, чтобы не держать в памяти гигантские массивы
-    for t_idx, time_val in enumerate(time_indices):
-        step_data = df_dens[df_dens['time_index'] == time_val]['d_dens'].values
-        print(step_data)
-        print(step_data.shape)
+    for t_idx, df_dens in tqdm(enumerate(pd.read_csv(files['data'], sep=r'\s+', header=None, names=column_names, 
+                      dtype={'time_index': int}, chunksize=chunksize)), desc="Time step"):        
+        step_data = df_dens['d_dens'].values
+        #print(step_data)
+        #print(step_data.shape)
         step_2D = np.zeros(shape=(N_rho, N_theta))
         start_index = 0
         theta_new = np.linspace(0, 2 * np.pi, N_theta, endpoint=False)
         for indx, _N_theta in enumerate(N_theta_1d):
             radial_slice = step_data[start_index : start_index + _N_theta]
             theta_old = np.linspace(0, 2 * np.pi, _N_theta, endpoint=False)
-            interpolated_slice = np.interp(theta_new, theta_old, radial_slice, period=2*np.pi)
+            interpolated_slice = np.interp(theta_new, theta_old, radial_slice, period= 2*np.pi)
             step_2D[indx,:] = interpolated_slice
             start_index = start_index + _N_theta
         ds_fluct[t_idx] = step_2D
-        plot_polar_2d(rho_1d, theta, step_2D)
-        break
+        #plot_polar_2d(rho_1d, theta, step_2D)
+        #break
